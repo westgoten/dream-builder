c372c1ffc9d139ed7761060448629645c1f4fbf6
westgoten <rodrigodeoliveira397@gmail.com>
2019-04-27 23:09:35 -0300

    Moved repo to git

---
+++ app/src/main/java/com/westgoten/dreambuilder/MainActivity.java
@@ -0,0 +1,16 @@
+package com.westgoten.dreambuilder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_calculation_type_selection);
+
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+    }
+}
